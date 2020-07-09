411d6d3e59e91bdaa66c8830609f9fbe364c4ab2
Yefim Vedernikoff <yefim323@gmail.com>
2020-07-09 16:37:46 -0700

    Update redis

--- app.rb
+++ app.rb
@@ -2,11 +2,14 @@ require 'sinatra'
 require 'redis'
 
 get '/pixel.png' do
-  redis = Redis.new
   redis.incr('count')
   send_file 'pixel.png', :type => :png
 end
 
 get '/count' do
   redis.get('count')
+end
+
+def redis
+  @redis ||= Redis.new(url: ENV["REDIS_URL"])
 end
